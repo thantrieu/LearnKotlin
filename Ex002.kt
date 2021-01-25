03047750a2117f52b44fbadd1add517e1cdec53a
Trieu Than <trieuthan92pro@gmail.com>
2021-01-25 17:00:12 +0700

    Add exercises 003

---
+++ Ex002.kt
@@ -0,0 +1,34 @@
+package braniumacademy
+
+fun main() {
+    var n: Int
+    var k: Int
+    n = getNFromKeyboard()
+    k = getKFromKeyboard(n)
+    var result = (n / k) * k
+    println(result)
+}
+
+private fun getKFromKeyboard(n: Int): Int {
+    var k: Int
+    while (true) {
+        println("Nhập số nguyên dương k < n: ")
+        k = readLine()!!.toInt()
+        if (k > 0 && k < n) {
+            break
+        }
+    }
+    return k
+}
+
+private fun getNFromKeyboard(): Int {
+    var n: Int
+    while (true) {
+        println("Nhập vào số nguyên dương n: ")
+        n = readLine()!!.toInt()
+        if (n > 0) {
+            break
+        }
+    }
+    return n
+}
---
+++ Ex003.kt
@@ -0,0 +1,27 @@
+package braniumacademy
+
+fun main() {
+    println("Nhập vào năm cần kiểm tra: ")
+    var year = readLine()!!.toInt()
+    var leapYear: Boolean = isLeapYear(year)
+    if (leapYear) {
+        println("YES")
+    } else {
+        println("NO")
+    }
+}
+
+fun isLeapYear(year: Int): Boolean {
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true
+            } else {
+                return false
+            }
+        } else {
+            return true
+        }
+    }
+    return false
+}
